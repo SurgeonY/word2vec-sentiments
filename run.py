37d24b588edf209893a8888e99243b84d2d71c1e
YPilipenko <surgeonyuriy@gmail.com>
2017-09-20 23:35:50 +0300

    some tunes and refactoring - remove code dups in run.py

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # gensim modules
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
+# from gensim.models.doc2vec import TaggedLineDocument
 from gensim.models import Doc2Vec
 
 # numpy
-import numpy
+# import numpy
 
 # shuffle
 from random import shuffle
@@ -13,7 +14,7 @@ from random import shuffle
 import logging
 import os.path
 import sys
-import cPickle as pickle
+# import cPickle as pickle
 
 program = os.path.basename(sys.argv[0])
 logger = logging.getLogger(program)
@@ -21,9 +22,11 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
 logging.root.setLevel(level=logging.INFO)
 logger.info("running %s" % ' '.join(sys.argv))
 
+
 class LabeledLineSentence(object):
 
     def __init__(self, sources):
+        self.sentences = []
         self.sources = sources
 
         flipped = {}
@@ -42,23 +45,29 @@ class LabeledLineSentence(object):
                     yield LabeledSentence(utils.to_unicode(line).split(), [prefix + '_%s' % item_no])
 
     def to_array(self):
-        self.sentences = []
+        for sent in self:
+            self.sentences.append(sent)
+        '''
         for source, prefix in self.sources.items():
             with utils.smart_open(source) as fin:
                 for item_no, line in enumerate(fin):
                     self.sentences.append(LabeledSentence(
                         utils.to_unicode(line).split(), [prefix + '_%s' % item_no]))
+        '''
         return self.sentences
 
     def sentences_perm(self):
-        shuffle(self.sentences)
-        return self.sentences
+        to_shuffle = self.sentences
+        shuffle(to_shuffle)
+        return to_shuffle
 
-sources = {'test-neg.txt':'TEST_NEG', 'test-pos.txt':'TEST_POS', 'train-neg.txt':'TRAIN_NEG', 'train-pos.txt':'TRAIN_POS', 'train-unsup.txt':'TRAIN_UNS'}
+sources = {'test-neg.txt':'TEST_NEG', 'test-pos.txt':'TEST_POS', 'train-neg.txt':'TRAIN_NEG', 'train-pos.txt':'TRAIN_POS'
+# , 'train-unsup.txt':'TRAIN_UNS'
+           }
 
 sentences = LabeledLineSentence(sources)
 
-model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=7)
+model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=4)
 
 model.build_vocab(sentences.to_array())
 
@@ -66,7 +75,8 @@ for epoch in range(50):
     logger.info('Epoch %d' % epoch)
     model.train(sentences.sentences_perm(),
                 total_examples=model.corpus_count,
-                epochs=model.iter,
-    )
+                epochs=model.iter
+                )
 
 model.save('./imdb.d2v')
+
