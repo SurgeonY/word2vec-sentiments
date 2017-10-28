f23840fe26af9739c55d17b68600a0dafb215e64
YPilipenko <surgeonyuriy@gmail.com>
2017-10-28 16:14:55 +0300

    some cleanup

--- prepare-texts.py
+++ prepare-texts.py
@@ -12,28 +12,35 @@ handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -
 log.addHandler(handler)
 
 '''
-Simple text files preparation. Merges raw movie review files into single one with the format 
-removing all punctuation and de-capitalizing. Converts everything to lower case and removing punctuation 
+Simple text files preparation. Merges raw text files into a single one with the format of one document per line.
+Converts everything to lower case and removes punctuation. 
 '''
 
 dir_in = './aclImdb/train/unsup/'
 file_out = './train-unsup.txt'
 
-def __main__():
+
+def main():
+    # Translator for stripping punctuation. Not stripping actually but replacing with space to prevent
+    # merging words into one. Anyway all multiple spaces will be normalized later.
     strip_table = str.maketrans({key: ' ' for key in string.punctuation})
 
+    log.info("Processing directory %s", dir_in)
     with open(file_out, mode='w', encoding='utf8') as f_out:
         for i, f_name in enumerate(os.listdir(dir_in)):
+            if not os.path.isfile(f_name):
+                continue
             if f_name.endswith(".txt"):
                 f_path = os.path.join(dir_in, f_name)
-                log.info(str(i) + ":" + f_path)
+                if i % 1000 == 0:
+                    log.info(str(i) + ":" + f_path)
 
                 # process the text to one string etc
                 txt = read_file(f_path)\
                     .replace('<br />', ' ')\
                     .lower()\
                     .translate(strip_table)   # removing punctuation
-                f_out.write(' '.join(txt.split()) + '\n')  # removing multiple spaces
+                f_out.write(' '.join(txt.split()) + '\n')  # removing multiple spaces, tabs and CRLFs
 
         log.info("*** Written %i bytes", f_out.tell())
 
@@ -43,4 +50,5 @@ def read_file(file_path):
         return f.read()
 
 
-__main__()
+if __name__ == '__main__':
+    main()
--- word2vec-sentiments.py
+++ word2vec-sentiments.py
@@ -15,6 +15,7 @@ from sklearn.linear_model import LogisticRegression
 import logging
 import sys
 
+
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
@@ -24,60 +25,63 @@ handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -
 log.addHandler(handler)
 
 
-class TaggedLineSentence(object):
+class TaggedLineDocs(object):
     def __init__(self, sources):
         self.sources = sources
         self.sentences = []
 
-        flipped = {}
-
-        # make sure that keys are unique
-        for key, value in sources.items():
-            if value not in flipped:
-                flipped[value] = [key]
+        # make sure that prefixes are unique - why do we need this??
+        prefixes = set()
+        for value in self.sources.values():
+            if value not in prefixes:
+                prefixes.add(value)
             else:
                 raise Exception('Non-unique prefix encountered')
 
-    def __iter__(self):
+        # load all in memory
         for source, prefix in self.sources.items():
-            with utils.smart_open(source) as fin:
+            with open(source, encoding="utf8") as fin:
                 for item_no, line in enumerate(fin):
-                    yield TaggedDocument(utils.to_unicode(line).split(), [prefix + '_%s' % item_no])
+                    self.sentences.append(TaggedDocument(utils.to_unicode(line).split(), [prefix + '_%s' % item_no]))
 
-    def to_array(self):
-        for s in self:
-            self.sentences.append(s)
-        return self.sentences
+    def __iter__(self):
+        return iter(self.sentences)
 
     def shuffle(self):
-        shuffled = list(self.sentences)
-        random.shuffle(shuffled)
-        return shuffled
+        random.shuffle(self.sentences)
+        return self
 
 
+# Start loading dataset and load/train from scratch a model
 log.info('source load')
-sources = {'test-neg.txt': 'TEST_NEG', 'test-pos.txt': 'TEST_POS', 'train-neg.txt': 'TRAIN_NEG',
-           'train-pos.txt': 'TRAIN_POS', 'train-unsup.txt': 'TRAIN_UNS'}
-
-log.info('TaggedDocument')
-sentences = TaggedLineSentence(sources)
+sources = {'test-neg.txt': 'TEST_NEG',
+           'test-pos.txt': 'TEST_POS',
+           'train-neg.txt': 'TRAIN_NEG',
+           'train-pos.txt': 'TRAIN_POS',
+           'train-unsup.txt': 'TRAIN_UNS'}
+documents = TaggedLineDocs(sources)
 
 log.info('Initializing D2V model')
-model = Doc2Vec(min_count=1, window=10, size=400, sample=1e-4, negative=5, workers=3, dm=0)
-model.build_vocab(sentences.to_array())
+epochs = 20
+vec_size = 400
 
-epochs = 50
-log.info('Training Epochs %i', epochs)
-for epoch in range(epochs):
-    log.info('EPOCH: {}'.format(epoch))
-    model.train(sentences.shuffle())
+# model = Doc2Vec(min_count=3, window=10, size=vec_size, sample=1e-4, negative=5, workers=4, dm=0, iter=epochs)
 
-log.info('Model Save')
-# model.save('./imdb.d2v')
-model = Doc2Vec.load('./imdb.d2v')
+# log.info('Pre-trained D2V Model Load')
+model = Doc2Vec.load('./imdb_new.d2v')
 
+# model.build_vocab(documents)
+
+# log.info('Training Epochs %i', epochs)
+# model.train(documents, total_examples=model.corpus_count, epochs=model.iter)
+
+# log.info('Model Save')
+# model.save('./imdb_new.d2v')
+
+
+# Classifying sentiment
 log.info('Sentiment')
-train_arrays = numpy.zeros((25000, 100))
+train_arrays = numpy.zeros((25000, vec_size))
 train_labels = numpy.zeros(25000)
 
 for i in range(12500):
@@ -90,7 +94,7 @@ for i in range(12500):
 
 log.info(train_labels)
 
-test_arrays = numpy.zeros((25000, 100))
+test_arrays = numpy.zeros((25000, vec_size))
 test_labels = numpy.zeros(25000)
 
 for i in range(12500):
